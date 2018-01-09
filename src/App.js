5c00b4c9b1630c51d43895c91ed402de5606faae
Alistair Kane <vplelmo@hotmail.co.uk>
2018-01-09 19:44:22 +0000

    links up chart file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Chart from './Components/Chart'
 
 class App extends Component {
   render() {
+    return(
+      <Chart/>
+    )
 
-    );
   }
 }
 
---
+++ src/Components/Chart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Chart extends React.Component {
+  render() {
+    return(
+      <h1>hi</h1>
+    )
+  }
+}
+
+export default Chart
