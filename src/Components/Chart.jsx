98b368ca66ec65cfda05844163d3512afd82f08d
Alistair Kane <vplelmo@hotmail.co.uk>
2018-01-09 20:17:26 +0000

    adds song jsx file

--- src/Components/Chart.jsx
+++ src/Components/Chart.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Songs from './Songs'
 
 class Chart extends React.Component {
   constructor(props){
@@ -10,12 +11,11 @@ class Chart extends React.Component {
 
   render() {
     return(
-      <h1>hi</h1>
+      <Songs/>
     )
   }
 
   componentDidMount(){
-    console.log('componentDidMount');
     const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
@@ -27,31 +27,13 @@ class Chart extends React.Component {
         let jsonString = xhr.responseText;
         let iTunesData = JSON.parse(jsonString);
         console.log(iTunesData);
+        this.setState(prevState => {
+          return {data: prevState.data.concat(iTunesData)};
+        });
       }
     })
     xhr.send();
- }
+  }
 }
-   // const oauth = "e47c5a1f1fd83eb8af0889a83adca667a633db80";
-   // for (let user of this.state.usernames){
-   //   let url = `https://api.github.com/users/${user}?access_token=${oauth}`;
-   //   const xhr = new XMLHttpRequest();
-   //   xhr.open('GET', url);
-   //   xhr.addEventListener("load", () => {
-   //     if (xhr.status !== 200){
-   //       return console.log("request failed");
-   //     } else {
-   //       console.log("request successful");
-   //       let jsonString = xhr.responseText;
-   //       let userData = JSON.parse(jsonString);
-   //       console.log(userData);
-   //       this.setState(prevState => {
-   //         return {profiles: prevState.profiles.concat(userData)};
-   //       });
-   //     }
-   //   });
-   //
-
-
 
 export default Chart
