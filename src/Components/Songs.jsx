841e241b11a271598d9f01c7926d48722293d37c
Alistair Kane <vplelmo@hotmail.co.uk>
2018-01-09 21:41:31 +0000

    styles css

--- src/Components/Songs.jsx
+++ src/Components/Songs.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 class Songs extends React.Component {
   render(){
-      console.log("profiles: ", this.props.songs);
-      return <h1>hi</h1>
+    if(!this.props.songs) return null
+    const top20 = this.props.songs.map((song, index) => {
+
+      let position = <p className="song-position">{index + 1}</p>
+      let artist = <p className="song-artist" value={index} key={index}>{song["im:artist"].label}</p>
+      let name = <p className="song-name">{song["im:name"].label}</p>
+      let image = <img className="song-images" src={song["im:image"][0].label} alt={song["im:name"].label}/>
+      let price = <p className="song-price">{song["im:price"].label}</p>
+
+      return <article className="song-article">{position}{image}{artist}{name}{price}</article>
+    })
+    return <section className="song-section">{top20}</section>
   }
 }
 
